4d27a2d266b3a2aacdac464fe10c74cca9f264c1
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-05-29 22:14:43 +0200

    Created Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let type = 'button';
+</script>
+
+<button
+	{type}
+	class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+	on:click
+>
+	<slot />
+</button>
