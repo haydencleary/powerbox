beb0a6180314a7953eae28e412cf16954b562e27
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-05-29 23:49:59 +0200

    Added Textarea components

---
+++ src/components/Textarea.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let id: HTMLTextAreaElement['id'];
+	export let name: HTMLTextAreaElement['name'];
+	export let placeholder: HTMLInputElement['placeholder'] = '';
+	export let value: HTMLInputElement['value'] = '';
+	export let describedby = '';
+</script>
+
+<textarea
+	rows="4"
+	{name}
+	{id}
+	{placeholder}
+	{value}
+	aria-describedby={describedby}
+	class="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded"
+/>
