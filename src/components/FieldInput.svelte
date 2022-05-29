c5191e93ad25fd64d226cc10bc6e603c5385e720
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-05-29 23:50:57 +0200

    Added Input components

---
+++ src/components/FieldInput.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Input from '$components/Input.svelte';
+	import Label from '$components/Label.svelte';
+
+	export let type: HTMLInputElement['type'] = 'text';
+	export let id: HTMLInputElement['id'];
+	export let name: HTMLInputElement['name'];
+	export let placeholder: HTMLInputElement['placeholder'] = '';
+	export let value: HTMLInputElement['value'] = '';
+	export let min: HTMLInputElement['min'] = '';
+	export let max: HTMLInputElement['max'] = '';
+	export let step: HTMLInputElement['step'] = '';
+	export let describedby = '';
+</script>
+
+<div>
+	<Label for={id}><slot /></Label>
+	<div class="mt-1">
+		<Input {type} {id} {name} {placeholder} {value} {min} {max} {step} {describedby} />
+	</div>
+	{#if $$slots.message}
+		<p class="mt-2 text-sm text-gray-500" id={describedby}><slot name="message" /></p>
+	{/if}
+</div>
---
+++ src/components/Input.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	// TODO find a way to allow undefined props and not insert them into the DOM
+	export let type: HTMLInputElement['type'] = 'text';
+	export let name: HTMLInputElement['name'];
+	export let id: HTMLInputElement['id'];
+	export let placeholder: HTMLInputElement['placeholder'] = '';
+	export let value: HTMLInputElement['value'] = '';
+	export let min: HTMLInputElement['min'] = '';
+	export let max: HTMLInputElement['max'] = '';
+	export let step: HTMLInputElement['step'] = '';
+	export let describedby = '';
+</script>
+
+<input
+	{type}
+	{name}
+	{id}
+	{placeholder}
+	{value}
+	{min}
+	{max}
+	{step}
+	aria-describedby={describedby}
+	class="shadow-sm focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded accent-blue"
+	class:shadow-none={type === 'range'}
+/>
