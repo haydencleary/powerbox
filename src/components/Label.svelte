05240b118639efe0b342bfbea2b0106e4ab31a92
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-05-29 23:49:23 +0200

    Added Label component

---
+++ src/components/Label.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	let htmlFor: HTMLLabelElement['htmlFor'];
+	export { htmlFor as for };
+</script>
+
+<label for={htmlFor} class="block text-sm font-medium text-gray-700"><slot /></label>
