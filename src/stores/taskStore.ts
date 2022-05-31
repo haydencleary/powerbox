39fac8ffb1a1766deccb2820ecb05d23df6bb1fd
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-05-31 22:12:30 +0200

    Made store persistent

--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -1,31 +1,58 @@
+import { browser } from '$app/env';
 import { writable } from 'svelte/store';
 
 import type { Task } from '$types/task';
 
-export const tasks = writable([] as Task[]);
+let initialTasks: Task[] = [];
+
+if (browser) {
+	const storedTasks = window.localStorage.getItem('tasks');
+
+	if (storedTasks) {
+		initialTasks = JSON.parse(storedTasks);
+	}
+}
+
+export const tasks = writable(initialTasks);
 
 export const createTask = (newTask: Task) => {
-	tasks.update((store) => [...store, { ...newTask, id: Date.now().toString() }]);
+	tasks.update((store) => {
+		const updated = [...store, { ...newTask, id: Date.now().toString() }];
 
-	// TODO update local storage
+		if (browser) {
+			window.localStorage.setItem('tasks', JSON.stringify(updated));
+		}
+
+		return updated;
+	});
 };
 
 export const updateTask = (updatedTask: Task) => {
-	tasks.update((store) =>
-		store.map((task) => {
+	tasks.update((store) => {
+		const updated = store.map((task) => {
 			if (task.id === updatedTask.id) {
 				return updatedTask;
 			}
 
 			return task;
-		})
-	);
+		});
 
-	// TODO update local storage
+		if (browser) {
+			window.localStorage.setItem('tasks', JSON.stringify(updated));
+		}
+
+		return updated;
+	});
 };
 
 export const deleteTask = (id: string) => {
-	tasks.update((store) => store.filter((task) => task.id !== id));
+	tasks.update((store) => {
+		const updated = store.filter((task) => task.id !== id);
 
-	// TODO update local storage
+		if (browser) {
+			window.localStorage.setItem('tasks', JSON.stringify(updated));
+		}
+
+		return updated;
+	});
 };
