d5896627b2cf1bebc255c3d487460db08d5b8f04
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-06-05 16:24:35 +0200

    Updated edit and delete buttons

--- src/components/ViewTask.svelte
+++ src/components/ViewTask.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 	import { page } from '$app/stores';
-
+	import { Menu, MenuButton, MenuItems, MenuItem, Transition } from '@rgossiaux/svelte-headlessui';
 	import Field from '$components/Field.svelte';
-
 	import EditIcon from '$icons/Edit.svelte';
 	import DeleteIcon from '$icons/Delete.svelte';
-
 	import { updateTask } from '$stores/taskStore';
-
 	import type { Task } from '$types/task';
 	import { TASK_STATUS_DONE, TASK_STATUS_TODO } from '$types/task';
 
@@ -41,7 +38,7 @@
 
 <article
 	id={task.id}
-	class={`relative hover:bg-blue-50 hover:bg-opacity-50 flex border-b border-slate-200 group overflow-hidden transition justify-center ${
+	class={`relative hover:bg-blue-50 hover:bg-opacity-50 flex border-b border-slate-200 group transition justify-center ${
 		isHighlighted ? 'shadow-inner shadow-blue-500/50' : ''
 	}`}
 >
@@ -50,14 +47,79 @@
 			<input type="checkbox" class="rounded" checked={isDone} on:change={handleStatusChange} />
 		</div>
 		<div class="ml-3 w-full space-y-3">
-			<p
-				class="font-medium"
-				class:text-slate-900={!isDone}
-				class:text-slate-500={isDone}
-				class:line-through={isDone}
-			>
-				{title}
-			</p>
+			<div class="flex justify-between items-start">
+				<p
+					class="font-medium"
+					class:text-slate-900={!isDone}
+					class:text-slate-500={isDone}
+					class:line-through={isDone}
+				>
+					{title}
+				</p>
+
+				<Menu
+					as="div"
+					class={({ open }) => `relative inline-block text-left ${open ? 'z-10' : ''}`}
+				>
+					<div>
+						<MenuButton
+							class="rounded-full flex items-center text-slate-400 hover:text-slate-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-100 focus:ring-blue-500"
+						>
+							<span class="sr-only">Open options</span>
+							<!-- Heroicon name: solid/dots-vertical -->
+							<svg
+								class="h-5 w-5"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								aria-hidden="true"
+							>
+								<path
+									d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
+								/>
+							</svg>
+						</MenuButton>
+					</div>
+
+					<Transition
+						enter="transition ease-out duration-100"
+						enterFrom="transform opacity-0 scale-95"
+						enterTo="transform opacity-100 scale-100"
+						leave="transition ease-in duration-75"
+						leaveFrom="transform opacity-100 scale-100"
+						leaveTo="transform opacity-0 scale-95"
+					>
+						<MenuItems
+							class="origin-top-right absolute right-0 mt-2 w-32 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+						>
+							<div class="py-1">
+								<MenuItem
+									class={({ active }) =>
+										`flex items-center px-4 py-2 text-sm cursor-pointer ${
+											active ? 'bg-slate-100 text-slate-900' : 'text-slate-700'
+										}`}
+									on:click={() => {
+										dispatch('edit', id);
+									}}
+								>
+									<EditIcon /><span class="ml-2">Edit</span>
+								</MenuItem>
+								<MenuItem
+									class={({ active }) =>
+										`flex items-center px-4 py-2 text-sm cursor-pointer ${
+											active ? 'bg-red-100 text-red-900' : 'text-red-700'
+										}`}
+									on:click={() => {
+										dispatch('delete', id);
+									}}
+								>
+									<DeleteIcon /><span class="ml-2">Delete</span>
+								</MenuItem>
+							</div>
+						</MenuItems>
+					</Transition>
+				</Menu>
+			</div>
 
 			{#if description}
 				<p class="mt-1 text-slate-500 text-sm whitespace-pre-line">
@@ -68,42 +130,26 @@
 			{#if !isDone}
 				<div class="flex gap-4">
 					<Field id={`${id}_impact`} label="Impact">
-						<meter id={`${id}_impact`} value={impact} min="0" max="10" low="3" high="7">
+						<meter id={`${id}_impact`} value={impact} min="1" max="10" low="3" optimum="5" high="7">
 							{impact}/10
 						</meter>
 					</Field>
 
 					<Field id={`${id}_urgency`} label="Urgency">
-						<meter id={`${id}_urgency`} value={urgency} min="0" max="10" low="3" high="7">
+						<meter
+							id={`${id}_urgency`}
+							value={urgency}
+							min="1"
+							max="10"
+							low="3"
+							optimum="5"
+							high="7"
+						>
 							{urgency}/10
 						</meter>
 					</Field>
 				</div>
 			{/if}
-		</div>
-
-		<!-- TODO hide this when done ? -->
-		<div
-			class="absolute top-0 bottom-0 right-0 translate-x-full opacity-0 group-hover:translate-x-0 group-hover:opacity-100 transition flex flex-col bg-blue-100 bg-opacity-40 backdrop-blur-sm px-4 py-7 gap-6 shadow-md"
-		>
-			<button
-				class="text-blue-500"
-				on:click={() => {
-					dispatch('edit', id);
-				}}
-			>
-				<span class="sr-only">Edit</span>
-				<EditIcon />
-			</button>
-			<button
-				class="text-red-500"
-				on:click={() => {
-					dispatch('delete', id);
-				}}
-			>
-				<span class="sr-only">Delete</span>
-				<DeleteIcon />
-			</button>
 		</div>
 	</div>
 </article>
