c552c14805499b50e0b6a632294b9548809f0cc0
haydencleary <5160414+haydencleary@users.noreply.github.com>
2022-06-30 21:48:57 +0200

    Added Github link

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -85,16 +85,12 @@
 						</button>
 					</div>
 
-					<div class="pt-5 pb-4">
+					<div class="flex flex-1 flex-col pt-5 pb-4">
 						<div class="flex-shrink-0 flex items-center px-4">
-							<img
-								class="h-8 w-auto"
-								src="https://tailwindui.com/img/logos/workflow-mark.svg?color=blue&shade=600"
-								alt="Workflow"
-							/>
+							<span style="font-size:22px" title="Powerbox">🔥</span>
 						</div>
-						<nav aria-label="Sidebar" class="mt-5">
-							<div class="px-2 space-y-1">
+						<nav aria-label="Sidebar" class="mt-5 flex flex-1 flex-col">
+							<div class="px-2 space-y-1 flex flex-1 flex-col justify-between">
 								<div
 									class="group p-2 rounded-md flex items-center text-base font-medium text-slate-600 hover:bg-slate-50 hover:text-slate-900"
 								>
@@ -115,6 +111,27 @@
 									</svg>
 									Tasks
 								</div>
+
+								<a
+									href="https://github.com/haydencleary/powerbox"
+									target="_blank"
+									rel="noopener noreferrer"
+									class="group p-2 rounded-md flex items-center text-base font-medium text-slate-600 hover:bg-slate-50 hover:text-slate-900"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="mr-4 h-6 w-6 text-slate-400 group-hover:text-slate-500"
+										fill="currentColor"
+										viewBox="0 0 24 24"
+									>
+										<path
+											fill-rule="evenodd"
+											d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+											clip-rule="evenodd"
+										/>
+									</svg>
+									Github
+								</a>
 							</div>
 						</nav>
 					</div>
@@ -152,15 +169,14 @@
 	<div class="hidden lg:flex lg:flex-shrink-0">
 		<div class="flex flex-col w-20">
 			<div class="flex-1 flex flex-col min-h-0 overflow-y-auto bg-blue-600">
-				<div class="flex-1">
+				<div class="flex flex-col flex-1">
 					<div class="bg-blue-700 py-4 flex items-center justify-center">
-						<img
-							class="h-8 w-auto"
-							src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
-							alt="Workflow"
-						/>
+						<span style="font-size:22px" title="Powerbox">🔥</span>
 					</div>
-					<nav aria-label="Sidebar" class="py-6 flex flex-col items-center space-y-3">
+					<nav
+						aria-label="Sidebar"
+						class="py-6 flex flex-1 flex-col items-center space-y-3 justify-between"
+					>
 						<div class="flex items-center p-4 rounded-lg text-blue-200 hover:bg-blue-700">
 							<!-- Heroicon name: outline/document-text -->
 							<svg
@@ -179,6 +195,27 @@
 							</svg>
 							<span class="sr-only">Tasks</span>
 						</div>
+
+						<a
+							href="https://github.com/haydencleary/powerbox"
+							target="_blank"
+							rel="noopener noreferrer"
+							class="flex items-center p-4 rounded-lg text-blue-200 hover:bg-blue-700"
+						>
+							<span class="sr-only">GitHub</span>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-6 w-6"
+								fill="currentColor"
+								viewBox="0 0 24 24"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</a>
 					</nav>
 				</div>
 				<!-- <div class="flex-shrink-0 flex pb-5">
@@ -203,11 +240,7 @@
 		<div class="lg:hidden">
 			<div class="bg-blue-600 py-2 px-4 flex items-center justify-between sm:px-6">
 				<div>
-					<img
-						class="h-8 w-auto"
-						src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
-						alt="Workflow"
-					/>
+					<span style="font-size:22px" title="Powerbox">🔥</span>
 				</div>
 				<div>
 					<button
